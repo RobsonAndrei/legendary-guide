609e9a6f69781e0d1145fa7ed903223266ff63cd
robsonsantos <robsonsantos@192.168.1.6>
2017-05-06 00:55:24 -0300

    AVL

---
+++ bst-2017-1/src/br/pucrs/ap3/trees/AvlTree.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2017, ALPRO III and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+
+package br.pucrs.ap3.trees;
+
+/***
+ * The {@code AVL Tree} class is...
+ * 
+ * @author robsonsantos
+ *
+ */
+
+public class AvlTree {
+
+	private AvlNode root;
+
+	class AvlNode {
+
+		public int key;
+		public AvlNode left, right;
+		public int height;
+	}
+
+	/**
+	 * 
+	 */
+	public AvlTree() {
+		root = null;
+	}
+
+	/***
+	 * 
+	 * @param value
+	 */
+
+	public void add(int value) {
+		root = add0(root, value);
+	}
+
+	/**
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public boolean contains(int value) {
+		return contains0(root, value);
+	}
+
+	/***
+	 * 
+	 * @param node
+	 * @param value
+	 * @return
+	 */
+	private AvlNode add0(AvlNode node, int value) {
+		if (node == null) {
+			AvlNode n = new AvlNode();
+			n.key = value;
+			n.left = null;
+			n.right = null;
+			n.height = 0;
+			return n;
+		}
+		if (node.key == value)
+			throw new RuntimeException("Valor existente!");
+
+		if (node.key < value) {
+			node.right = add0(node.right, value);
+			// Todo: AVL
+			// Qual o fator de equilibrio? Delta
+		} else {
+			node.left = add0(node.left, value);
+			// Todo: AVL
+		}
+		// Atualizar altura em cada nodo....
+		int hl = getHeight(node.left);
+		int hr = getHeight(node.right);
+		int h = 1 + Math.max(hl, hr);
+		node.height = h;
+		return node;
+	}
+
+	/**
+	 * 
+	 * @param node
+	 * @return
+	 */
+	private int getHeight(AvlNode node) {
+		return node == null ? -1 : node.height;
+	}
+
+	/**
+	 * 
+	 * @param node
+	 * @param value
+	 * @return
+	 */
+	private boolean contains0(AvlNode node, int value) {
+		if (node == null)
+			return false;
+		if (node.key == value)
+			return true;
+		if (node.key < value)
+			return contains0(node.right, value);
+		return contains0(node.left, value);
+
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public int size() {
+		return size0(root);
+	}
+
+	/**
+	 * 
+	 * @param node
+	 * @return
+	 */
+	private int size0(AvlNode node) {
+		if (node == null)
+			return 0;
+
+		return 1 + size0(node.left) + size0(node.right);
+	}
+
+	/**
+	 * 
+	 */
+	public String toString() {
+		return toString0(root);
+	}
+
+	/**
+	 * 
+	 * @param node
+	 * @return
+	 */
+	private String toString0(AvlNode node) {
+		if (node == null)
+			return " # ";
+
+		return toString0(node.left) + String.format(" %d ", node.key) + toString0(node.right);
+	}
+
+	/**
+	 * Reports level number for every node.
+	 * 
+	 * Prints key and level for every node. Root node is at level 0.
+	 */
+	public void reportLevel() {
+		reportLevel0(root, 0);
+	}
+
+	/**
+	 * 
+	 * @param node
+	 * @param level
+	 */
+	private void reportLevel0(AvlNode node, int level) {
+		if (node == null) {
+			System.out.printf("Key=%d Level=%d!\n", node.key, level);
+
+			reportLevel0(node.left, level + 1);
+			reportLevel0(node.right, level + 1);
+		}
+	}
+
+	/**
+	 * Reports height number for every node.
+	 * 
+	 * Prints key and height for every node. Leaf nodes are at height 0.
+	 */
+	public void reportHeight() {
+		reportHeight0(root);
+	}
+
+	/**
+	 * 
+	 * @param node
+	 * @return
+	 */
+	private int reportHeight0(AvlNode node) {
+		if (node == null) {
+			return -1;
+		}
+		int hl = reportHeight0(node.left);
+		int hr = reportHeight0(node.right);
+		int height = Math.max(hl, hr) + 1;
+		System.out.printf("key=%d Height=%d!\n", node.key, height);
+		return height;
+	}
+
+}

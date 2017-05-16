06e650b1fd042e76a0d5a072cb394677e15f22e3
robsonsantos <robsonsantos@10.132.253.39>
2017-05-15 21:18:05 -0300

    Testing TDD

---
+++ bst-2017-1/src/Heap/HeapSort.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright
+ */
+
+package Heap;
+
+public class HeapSort {
+
+	public HeapSort() {
+
+	}
+
+	/***
+	 * 
+	 * @param v
+	 */
+	public void heapsort(int v[]) {
+		buildMaxHeap(v);
+		int size = v.length;
+		for (int i = v.length - 1; i > 0; i--) {
+			swap(v[0], v[i]);
+			maxHeapify(v, 0, --size);
+
+		}
+	}
+
+	/***
+	 * 
+	 * @param i
+	 * @param j
+	 */
+	private void swap(int i, int j) {
+		int aux = i;
+		i = j;
+		j = aux;
+
+	}
+
+	/***
+	 * 
+	 * @param v
+	 * @param i
+	 * @param size
+	 */
+	private void maxHeapify(int[] v, int i, int size) {
+		int largest;
+		int l = (2 * i) + 1;
+		int r = l + 1;
+
+		if ((l < size) && (v[l] > v[i]))
+			largest = 1;
+		else
+			largest = i;
+		if ((r < size) && (v[r] > v[largest]))
+			largest = r;
+		if (largest != i) {
+			swap(v[i], v[largest]);
+			maxHeapify(v, largest, size);
+		}
+
+	}
+
+	/***
+	 * 
+	 * @param v
+	 */
+	private void buildMaxHeap(int[] v) {
+		for (int i = (v.length / 2) - 1; i >= 0; i--) {
+			maxHeapify(v, i, v.length);
+		}
+
+	}
+
+	public static boolean isMaxHeap(int[] v) {
+		// TODO Verificar se eh um max heap!
+
+		// int index = v.length;
+		if (v == null)
+			throw new IllegalArgumentException("Error value null");
+		for (int i = 0; i < v.length / 2 - 1; i++) {
+			int left = 2 * i + 1;
+			if (left < v.length && v[i] < v[left])
+				return false;
+			int right = 2 * i + 2;
+			if (right < v.length && v[i] < v[right])
+				return false;
+		}
+
+		return true;
+	}
+
+}
---
+++ bst-2017-1/src/br/pucrs/ap3/trees/AvlTreeTest.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package br.pucrs.ap3.trees;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author robsonsantos
+ *
+ */
+public class AvlTreeTest {
+	private AvlTree avl;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link br.pucrs.ap3.trees.AvlTree#AvlTree()}.
+	 */
+	@Test
+	public void testAvlTree() {
+	avl = new AvlTree();
+		
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link br.pucrs.ap3.trees.AvlTree#add(int)}.
+	 */
+	@Test
+	public void testAdd() {
+		avl.add(10);
+		assertTrue(avl.contains(10));
+	}
+
+	/**
+	 * Test method for {@link br.pucrs.ap3.trees.AvlTree#contains(int)}.
+	 */
+	@Test
+	public void testContains() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link br.pucrs.ap3.trees.AvlTree#size()}.
+	 */
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link br.pucrs.ap3.trees.AvlTree#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link br.pucrs.ap3.trees.AvlTree#reportLevel()}.
+	 */
+	@Test
+	public void testReportLevel() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link br.pucrs.ap3.trees.AvlTree#reportHeight()}.
+	 */
+	@Test
+	public void testReportHeight() {
+		fail("Not yet implemented");
+	}
+
+}
